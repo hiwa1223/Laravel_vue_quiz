7960a8535931fe562f836e5fe4b1d57d3934a8ec
hiwa1223 <57945054+hiwa1223@users.noreply.github.com>
2020-06-17 14:43:14 +0900

    ホーム画面のお知らせAPI連携を実装する

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
-import router from './router.js'
-import SocialSharing from 'vue-social-sharing'
-
-Vue.use(SocialSharing);
-
-new Vue({
-  router: router, // routerにはrouter.jsファイルを設定します
- }).$mount('#app') // routerを適用する要素を設定(マウント)します
+ import Vue from 'vue'
+ import router from './router.js'
+ import SocialSharing from 'vue-social-sharing'
+ import axios from 'axios' // 追加
+ 
+ Vue.prototype.$http = axios; // 追加
+ 
+ Vue.use(SocialSharing);
+ 
+ new Vue({
+   router: router,
+  }).$mount('#app')
